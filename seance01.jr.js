489b2b034a997ac7c44c62d934752e116f931fb6
aymenleo <156076456+aymenleo@users.noreply.github.com>
2024-01-30 09:37:58 +0100

    test git

---
+++ seance01.jr.js
@@ -0,0 +1,29 @@
+console.log("JAVA SCRIPT WORLD")
+
+function leoo(name){
+  console.log("its me "+ name)
+}
+leoo("othman")
+leoo("aymane")
+leoo("hiba")
+ function aymm(nombre1,nombre2){
+    return nombre1 + nombre2
+ }
+ let sm 
+ console.log(aymm(20,100))
+ const PI = 3.14
+ function aymn(){
+    let nom;
+    let prenom;
+    let age;
+    nom="AYMEN"
+    prenom= "LEO"
+    age=21
+    if (age <22){
+        var branche;
+        branche="DD"
+    }
+    console.log(nom)
+    console.log(branche)
+ }
+ aymn()
